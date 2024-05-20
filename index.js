1eb97080aa69c9b5462554d17459d735bcf27e78
Mark Homchik <markhomchik4@gmail.com>
2024-05-19 21:34:50 -0500

    working index.js

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-const inquirer = require('inquirer');
 const fs = require('fs');
+const questions = require('./lib/questions.js');
+const fileName = "./examples/logo.svg";
+const setShape = require('./lib/setShape.js');
+
+// Wrapping the main logic inside an async function to use dynamic import
+async function initialize() {
+    try {
+        // Dynamically import inquirer as it's an ES module
+        const inquirer = await import('inquirer');
+
+        // Prompt the user with questions
+        const response = await inquirer.default.prompt(questions);
+        
+        // Create logo with the response
+        createLogo(response);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function createLogo(response) {
+    const svg = setShape(response);
+    fs.writeFile(fileName, svg, () => console.log('Generated logo.svg'));
+}
+
+// Start the initialization
+initialize();
